c71087f614e337830133ec0a3524c07fb2d927cb
Dan McCarthy <daniel.c.mccarthy@gmail.com>
2026-02-25 10:40:46 -0500

    package-lint fixes.

--- rtf-view.el
+++ rtf-view.el
@@ -1,8 +1,10 @@
 ;;; rtf-view.el --- View Rich Text Format files -*- lexical-binding: t; -*-
 
 ;; Author: Dan McCarthy <daniel.c.mccarthy@gmail.com>
+;; URL: https://github.com/danielcmccarthy/rtf-view.git
 ;; Keywords: files data
 ;; Package-Version: 1.0
+;; Package-Requires: ((emacs "24.1"))
 
 ;;; Commentary:
 ;; Display RTF documents in all of their formatted glory. Editing isn't possible yet.
@@ -17,7 +19,7 @@
 
 ;;; Code:
 
-(defcustom rtf-unrtf-executable
+(defcustom rtf-view-unrtf-executable
   "unrtf"
   "Path to the unrtf executable used by `rtf-view'."
   :type '(string)
@@ -26,12 +28,12 @@
 (defun rtf-view ()
   "Format the current buffer as an RTF document."
   (interactive)
-  (unless (locate-file rtf-unrtf-executable exec-path)
-    (error  "Could not find %s" rtf-unrtf-executable))
+  (unless (locate-file rtf-view-unrtf-executable exec-path)
+    (error  "Could not find %s" rtf-view-unrtf-executable))
   (message "Formatting %s with unrtf..." (buffer-name))
   (shell-command-on-region (point-min)
                            (point-max)
-                           (format "%s --nopict" rtf-unrtf-executable)
+                           (format "%s --nopict" rtf-view-unrtf-executable)
                            t
                            t)
   (let ((dom (libxml-parse-html-region (point-min)
